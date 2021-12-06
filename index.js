72b97be5b5a1033bcd58407a2193b4750702b724
marwaismail99 <ismailmarwa.563@gmail.com>
2021-12-06 16:45:35 +0000

    task 2d completed

--- index.js
+++ index.js
@@ -31,16 +31,18 @@ function describeItem (item) {
   }
   
   describeItem(myCollection[0]);
-//   2c. Create a function called `describeItem`, which should take in an item as a parameter 
-// (the argument handed to this function would be an item from our collection). The function should 
-// `console.log` a message according to how many of the item you have.
-// If there's only one of the item in your collection, it should log "I have a `name`. 
-// Here's what I like about it: `whatILike`". If you have more than one of it, the message 
-// should log "I have `count` `name`s. Here's what I like about them: `whatILike`".
 
-// For example, the result of calling the `describeItem` function with the first item in our collection would be:
-// _"I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"_
+//   👉 2d. Now make a function called describeCollection that takes in an array as a parameter. The function should loop through the array, and for each item, it should call the describeItem function so it displays a message according to how many you have in your collection.
 
-// Call your function below where you've defined it, handing in the first item in `myCollection`.
+// Call your function below where you've defined it, handing in the myCollection array.
 
-// Now run the file again (using `node index.js`);
+// Run the file again to see the output and check that it outputs the correct message for each item in your collection (i.e. each object in your myCollection array).
+
+function describeCollection(myCollection) {
+    for (i = 0; i < myCollection.length; i++) {
+        describeItem(myCollection[i]);
+    }
+
+}
+
+describeCollection(myCollection);
